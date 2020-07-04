6d683e285696b4ad40eaa592c59a6e9ae7ea2947
nimishbajaj <nmsbajaj@gmail.com>
2020-07-04 13:25:26 +0530

    DifferentSummands and LargestNumber

---
+++ src/DifferentSummands.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class DifferentSummands {
+    private static List<Integer> optimalSummands(int n) {
+        List<Integer> summands = new ArrayList<Integer>();
+        //write your code here
+
+        // the minimum that is feasible here is that the numbers should be in the form n(n+1)/2
+        int next = 1;
+        while (n >= next) {
+            n -= next;
+            summands.add(next++);
+        }
+        if (n > 0) {
+            summands.add(summands.remove(summands.size() - 1) + n);
+        }
+        return summands;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        List<Integer> summands = optimalSummands(n);
+        System.out.println(summands.size());
+        for (Integer summand : summands) {
+            System.out.print(summand + " ");
+        }
+    }
+}
+
---
+++ src/LargestNumber.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class LargestNumber {
+    private static String largestNumber(String[] a) {
+        //write your code here
+
+
+        Arrays.sort(a, new Comparator<String>() {
+            @Override
+            public int compare(String x, String y) {
+                return (y+x).compareTo(x+y);
+            }
+        });
+
+//        Arrays.sort(a);
+
+        StringBuilder result = new StringBuilder();
+        for (String s : a) {
+            result.append(s);
+        }
+
+//        System.out.println(Arrays.toString(a));
+
+        return result.toString();
+//        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String[] a = new String[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.next();
+        }
+        System.out.println(largestNumber(a));
+    }
+}
+
